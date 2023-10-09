b5226aefabf23c9ca6a419156cfb52c0249659a2
robots4life <34193243+robots4life@users.noreply.github.com>
2023-10-09 17:12:25 +0200

    :question:

--- svelte/src/hooks.server.ts
+++ svelte/src/hooks.server.ts
@@ -7,10 +7,11 @@ export const handle: Handle = async ({ event, resolve }) => {
 	console.log('\n');
 	console.log('hooks handle function');
 	console.log(new Date());
-	// console.log(event.request);
+	console.log(event.request.url);
 
 	if (event.url.pathname.startsWith('/api')) {
-		return api.handle(event.request);
+		console.log('elysia returns data');
+		return await api.handle(event.request);
 	}
 
 	const response = await resolve(event);
--- svelte/src/routes/no-api/+page.server.ts
+++ svelte/src/routes/no-api/+page.server.ts
@@ -1,30 +1,20 @@
 import { edenTreaty } from '@elysiajs/eden';
 import type { API } from '$lib/elysia';
-
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ route, fetch }) => {
 	//
 	console.log('\n');
-	console.log('/api load function');
+	console.log('/no-api load function');
 	console.log(new Date());
 	console.log(route);
-
-	// const request = await fetch('/api');
-	// const response = await request.json();
-	// return { response };
-
-	// how could I use EdenTreaty with the custom Svelte Fetch ??
-	// https://kit.svelte.dev/docs/web-standards#fetch-apis
-	// https://kit.svelte.dev/docs/load#making-fetch-requests
-	// const client = edenTreaty<API>('/api');
 
 	// https://discord.com/channels/1044804142461362206/1044807567022493766/1160931471301824592
 	const client = edenTreaty<API>('/', { fetcher: fetch });
 
 	const request = await client.api.get();
+
 	const response = request;
-	console.log(response.data);
 
 	return response.data;
 };
